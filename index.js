aee7a6cf55ed7a770263dbf2bc8a9181ac96f846
anwarul1 <anwarjewel007@gmail.com>
2022-04-26 06:41:08 +0600

    makeing node server add database add clint site add do some CRD operation from CURD operation

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rdxoe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db('geniusCar').collection('service');
+
+    // API for all service
+    app.get('/service', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const service = await cursor.toArray();
+      res.send(service);
+    });
+
+    // API for single service
+    app.get('/service/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    // POST
+    app.post('/service', async (req, res) => {
+      const newService = req.body;
+      const result = await serviceCollection.insertOne(newService);
+      res.send(result);
+    });
+
+    // DELETE
+    app.delete('/service/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+    // open it if needed....!!!
+    // await client.close()
+  }
+}
+run().catch(console.dir);
+app.get('/', (req, res) => {
+  res.send('Running Genius Server');
+});
+
+app.listen(port, () => {
+  console.log(`Node Server Running: ${port}`);
+});
+
+// DB_USER=geniusUser
+// DB_PASSWORD=RUxVmEnAWFIyYzLS
